c0ef02a916ea305ea44abd35abdb872b90a4b2fe
Iuri Sampaio <iurisampaio18@gmail.com>
2020-05-23 03:34:29 -0300

    update

--- um.js
+++ um.js
@@ -1,7 +1,10 @@
-const chave = '96988444ffe703373ca42f07efb5b6fc'
-const baseURL = 'https://api.themoviedb.org/3/'
+const chave = '96988444ffe703373ca42f07efb5b6fc';
+const baseURL = 'https://api.themoviedb.org/3/';
 
-let config = async() =>{
+let palavraChave = document.getElementById("palavraChave");
+const $info = document.getElementById('movies');
+
+let config = async ( ) => {
     try{
     let url = `${baseURL}configuration?api_key=${chave}`
     const confgApi= await fetch(url)
@@ -11,7 +14,20 @@ let config = async() =>{
       console.warn('erro na api')
     }
 }
-
+const pesquisa = async ( ) => {
+  let url = `${baseURL}search/movie?api_key=${chave}&language=pt-br&query=${palavraChave.value.trim()}`
+  let headers= new Headers ({
+      'Accept': 'application/json'
+  })
+  try{
+    const pegaApi = await fetch(url,headers)
+    const json = await pegaApi.json()
+    const resultados = json.results
+    return resultados ? preenche(resultados) : naoAcho(palavraChave)
+  }catch{
+    console.warn('Alguns resultados estão incompletos')
+  }   
+}
 const resultados = [{
     'title':"<div class=''></div>",
     'vote_average':"<div class=''></div>",
@@ -20,17 +36,14 @@ const resultados = [{
     'overview':"<div class=''></div>"
     
 }]
+const esconde = ( $i )  =>  $i.style.display = "none"
 
-const esconde = (i) =>{
-  i.classList.add(spinner)
-}
-
-const preenche = (resultados) =>{
-  //  $info.removeChild($info.firstChild)
+const preenche = ( resultados ) => {
+        // esconde($info.parentElement.lastChild.previousSibling)//esse caminho faz todos os filmes desaparecerem
 
     for($i=0; $i<=resultados.length;$i++){
-     
         resultados[$i].poster_path ? console.log('EXISTE IMG') : esconde(resultados[$i].poster_path)
+
         const panel= `
             <div class='conteinerzinho'>
                 <div class='filme'>
@@ -59,76 +72,18 @@ const preenche = (resultados) =>{
        
         const $container= document.createElement('div');
         $container.innerHTML = panel;
-        const $info = document.getElementById('movies');
+        
         $info.appendChild($container);
-      
-        const openModalButtons = document.querySelectorAll('[data-modal-target]')
-        const closeModalButtons = document.querySelectorAll('[data-close-button]')
-        const overlay = document.getElementById('overlay')
+
         
-        openModalButtons.forEach(button => {
-          button.addEventListener('click', () => {
-            const modal = document.querySelector(button.dataset.modalTarget)
-            openModal(modal)
-          })
-        })
-        
-        overlay.addEventListener('click', () => {
-          const modals = document.querySelectorAll('.modal.active')
-          modals.forEach(modal => {
-            closeModal(modal)
-          })
-        })
-        
-        closeModalButtons.forEach(button => {
-          button.addEventListener('click', () => {
-            const modal = button.closest('.modal')
-            closeModal(modal)
-          })
-        })
-        
-        const openModal = (modal) => {
-          if (modal == null) return
-          modal.classList.add('active')
-          overlay.classList.add('active')
-        }
-        
-        const closeModal = (modal) => {
-          if (modal == null) return
-          modal.classList.remove('active')
-          overlay.classList.remove('active')
-        }
-     
+        modal()
     }
-   
 
-//const palavraChave = document.getElementById('palavraChave').value
-//palavraChave.trim()
-//  palavraChave.addEventListener("change", console.log(palavraChave))
 } 
-
-const pesquisa = async (palavraChave) =>{
-  
-          let url = `${baseURL}search/movie?api_key=${chave}&language=pt-br&query=${palavraChave}`
-          let headers= new Headers ({
-              'Accept': 'application/json'
-          })
-          try{
-            const pegaApi = await fetch(url,headers)
-            const json = await pegaApi.json()
-            const resultados = json.results
-            resultados ? preenche(resultados) : naoAcho(palavraChave)
-            resultados.removeChild(resultados)
-          }catch{
-            console.warn('tem algo errado, só não sei oque kkk')
-          }   
-}
-
-
-const naoAcho = (palavraChave) =>{
+const naoAcho = ( palavraChave ) => {
     const  output = `
       <div class="erro">
-        <h1>Desculpe ${palavraChave} não foi encontrado </h1>
+        <h1>Desculpe ${palavraChave.value} não foi encontrado </h1>
       </div>
     `
     const $erro = document.createElement('div') 
@@ -136,5 +91,50 @@ const naoAcho = (palavraChave) =>{
     const $saierro =document.getElementById('movies')
     $saierro.appendChild($erro)
 }
-//preenche(resultados)
+const modal = ( ) => {
+
+  const openModalButtons = document.querySelectorAll('[data-modal-target]')
+  const closeModalButtons = document.querySelectorAll('[data-close-button]')
+  const overlay = document.getElementById('overlay')
+
+  openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+
+  const openModal = (modal) => {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+
+  const closeModal = (modal) => {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+}
+
 document.addEventListener('DOMContentLoaded', config);
+palavraChave.addEventListener('keypress',pesquisa)
+
+
+   
+
+ 
